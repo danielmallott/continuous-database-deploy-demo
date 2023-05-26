b2ce1e32b111027d54e9c1b4e9314024bcc74744
Dan Mallott <daniel.mallott@visma.com>
2023-05-26 15:39:56 +0200

    SSDT Demo #1

--- src/ssdt/LegoDatabase.SqlServer/PostDeployment.sql
+++ src/ssdt/LegoDatabase.SqlServer/PostDeployment.sql
@@ -10,6 +10,6 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 
--- :r .\PostDeploymentScripts\LoadPartsSSDTDemoTable.sql
+:r .\PostDeploymentScripts\LoadPartsSSDTDemoTable.sql
 -- :r .\PostDeploymentScripts\RenameParts_SSDT_DemoTable.sql
 -- :r .\PostDeploymentScripts\Rename_PartsSSDTDemo_Material_Column.sql
